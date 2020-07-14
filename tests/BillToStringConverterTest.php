896f4047ab00b1344644984c646a603656122a96
Eduard Dunareanu <eduard@alpenedv.at>
2020-07-14 14:00:50 +0200

    Improve tests, fix line endings

--- tests/BillToStringConverterTest.php
+++ tests/BillToStringConverterTest.php
@@ -26,13 +26,13 @@ class BillToStringConverterTest extends TestCase
         $bill->setAmount('EUR1456.89');
         $bill->setPaymentReference('457845789452');
         $bill->setReasonForPayment('Diverse Autoteile, Re 789452 KN 457845');
-        
+
         $expected = file_get_contents(__DIR__ . '/Fixtures/official-example-2.txt');
-        
+
         $converter = new BillToStringConverter();
         $this->assertSame($expected, $converter->convert($bill));
     }
-    
+
     /**
      * Test the BCD data generation, see doc example 2.
      */
@@ -47,15 +47,16 @@ class BillToStringConverterTest extends TestCase
         $bill->setAmount('EUR1337.99');
         $bill->setPaymentReference('R2020/1938');
         $bill->setReasonForPayment('Handshakomat Über 3000');
-        
+
         $expected = file_get_contents(__DIR__ . '/Fixtures/example-1.txt');
-        
+
         $converter = new BillToStringConverter();
         $this->assertSame($expected, $converter->convert($bill));
     }
-    public function testInValidConvertExample2()
+
+    public function testInvalidConvertExample2()
     {
-        
+
         $this->expectException(WrongNumberException::class);
         $bill = new Bill();
         $bill->setVersion(Bill::VERSION_2);
@@ -67,9 +68,10 @@ class BillToStringConverterTest extends TestCase
         $bill->setAmount('EUR1456.89');
         $bill->setPaymentReference('457845789452');
         $bill->setReasonForPayment('Diverse Autoteile, Re 789452 KN 457845');
-        
+
         $converter = new BillToStringConverter();
     }
+
     public function testConvertExample3()
     {
         $bill = new Bill();
@@ -82,12 +84,13 @@ class BillToStringConverterTest extends TestCase
         $bill->setAmount('EUR12345689.01');
         $bill->setPaymentReference('35ZeichenLangeREFzurZuordnungBeimBe');
         $bill->setReasonForPayment('Netter Text für den Zahlenden, damit dieser weiß, was er zahlt und auc');
-        
+
         $expected = file_get_contents(__DIR__ . '/Fixtures/example-3.txt');
-        
+
         $converter = new BillToStringConverter();
         $this->assertSame($expected, $converter->convert($bill));
     }
+
     public function testConvertExample4()
     {
         $bill = new Bill();
@@ -100,9 +103,9 @@ class BillToStringConverterTest extends TestCase
         $bill->setAmount('EUR1456.89');
         $bill->setPaymentReference('457845789452');
         $bill->setReasonForPayment('Diverse Autoteile, Re 789452 KN 457845');
-        
+
         $expected = file_get_contents(__DIR__ . '/Fixtures/example-4.txt');
-        
+
         $converter = new BillToStringConverter();
         $this->assertSame($expected, $converter->convert($bill));
     }
@@ -117,12 +120,13 @@ class BillToStringConverterTest extends TestCase
         $bill->setAmount('EUR12345689.01');
         $bill->setPaymentReference('35ZeichenLangeREFzurZuordnungBeimBe');
         $bill->setReasonForPayment('Netter Text für den Zahlenden, damit dieser weiß, was er zahlt und auc');
-        
+
         $expected = file_get_contents(__DIR__ . '/Fixtures/example-5.txt');
-        
+
         $converter = new BillToStringConverter();
         $this->assertSame($expected, $converter->convert($bill));
     }
+
     public function testConvertExample6()
     {
         $bill = new Bill();
@@ -136,15 +140,16 @@ class BillToStringConverterTest extends TestCase
         $bill->setPaymentReference('35ZeichenLangeREFzurZuordnungBeimBe');
         $bill->setReasonForPayment('Netter Text für den Zahlenden, damit dieser weiß, was er zahlt und auc');
         $bill->setUserNote('Danke Für ihren Einkauf.');
-        
+
         $expected = file_get_contents(__DIR__ . '/Fixtures/example-6.txt');
-        
+
         $converter = new BillToStringConverter();
         $this->assertSame($expected, $converter->convert($bill));
     }
+
     public function testConvertTooLongString()
     {
-        
+
         $bill = new Bill();
         $bill->setVersion(Bill::VERSION_1);
         $bill->setDecodingNumber(1);
@@ -156,8 +161,7 @@ class BillToStringConverterTest extends TestCase
         $bill->setPaymentReference('35ZeichenLangeREFzurZuordnungBeimBe');
         $bill->setReasonForPayment(str_repeat('X', 140));
         $bill->setUserNote(str_repeat('X', 70));
-        
-      
+
         $this->expectException(WrongTextFormatException::class);
         $converter = (new BillToStringConverter())->convert($bill);
     }
